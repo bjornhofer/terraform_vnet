d42e8b6f90e174224896b413f043d1812d3d5bd4
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-01-25 15:18:48 +0100

    fixed typo

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "main" {
   name                = var.vnet_name
   location            = var.vnet_location
-  resource_group_name = var.vnet_ressource_group_name
+  resource_group_name = var.vnet_resource_group_name
   address_space       = var.vnet_address_space
 }
 
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "main" {
   for_each             = var.vnet_subnet_details == null ? { "default" = var.vnet_address_space } : var.vnet_subnet_details
   name                 = each.key
   virtual_network_name = azurerm_virtual_network.main.name
-  resource_group_name  = var.vnet_ressource_group_name
+  resource_group_name  = var.vnet_resource_group_name
   address_prefixes     = each.value
   depends_on = [
     azurerm_virtual_network.main

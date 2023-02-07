185204bacd3a97f1b7dd46995fb423fbc31d4eff
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-02-07 08:25:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  features {}
+  alias = "vnet"
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+} 
+
 resource "azurerm_virtual_network" "main" {
   name                = var.vnet_name
   location            = var.vnet_location

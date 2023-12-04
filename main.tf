34c81b0c7a93d2e6d951759f2246386cb052ccea
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-12-04 14:55:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  suffix = [ "${var.vnet_name_suffix}" ]
+  prefix = [ "${var.vnet_name_prefix}" ]
+}
+
 resource "azurerm_virtual_network" "main" {
-  name                = var.vnet_name
+  name                = length(var.vnet_name) < 0 ? modul.naming.virtual_network.result : var.vnet_name
   location            = var.vnet_location
   resource_group_name = var.vnet_resource_group_name
   address_space       = var.vnet_address_space
